897f22d87b0ca8624b7ac41a7d860c35ac90a358
Dinesh Sivan <dineshsivan169@gmail.com>
2022-10-14 12:11:16 +0800

    enabled testing

---
+++ src/test/java/com/dinesh/intrvw/AgentRepoTest.java
@@ -0,0 +1,9 @@
+package com.dinesh.intrvw;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@DataJpaTest
+public class AgentRepoTest {
+
+}
--- src/test/java/com/dinesh/intrvw/IntrvwApplicationTests.java
+++ src/test/java/com/dinesh/intrvw/IntrvwApplicationTests.java
@@ -1,9 +1,15 @@
 package com.dinesh.intrvw;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class IntrvwApplicationTests {
+
+
+	@Test
+	void contextLoads() {
+	}
 
 
 }
