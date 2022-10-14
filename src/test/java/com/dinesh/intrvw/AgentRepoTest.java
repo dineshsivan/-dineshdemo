17eff8b9bb31559f909dfa7837cdcf57bc43eb22
Dinesh Sivan <dineshsivan169@gmail.com>
2022-10-14 13:04:28 +0800

    removed unused annotation

--- src/test/java/com/dinesh/intrvw/AgentRepoTest.java
+++ src/test/java/com/dinesh/intrvw/AgentRepoTest.java
@@ -1,9 +1,5 @@
 package com.dinesh.intrvw;
 
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@DataJpaTest
 public class AgentRepoTest {
 
 }
