38b8f520879a234d2aa89bced811b90174aef05c
Dinesh Sivan <dineshsivan169@gmail.com>
2023-05-04 15:12:54 +0800

    added new code

---
+++ src/main/java/com/dinesh/intrvw/ImageProcessor.java
@@ -0,0 +1,97 @@
+package com.dinesh.intrvw;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import marvin.image.MarvinImage;
+import marvin.io.MarvinImageIO;
+import marvinplugins.MarvinPluginCollection;
+
+public class ImageProcessor {
+	public static void main(String[] args) throws IOException {
+		System.setProperty("http.agent", "Chrome");
+		int splitWidth = 260;
+
+		MarvinImage sourceImg = MarvinImageIO.loadImage("E:\\Test Img\\Source1.png");
+		MarvinPluginCollection.blackAndWhite(sourceImg, 150);
+		spliAndWrite(sourceImg, "E:\\Test Img\\stampOnly", splitWidth,"jpg");
+		combineAndWrite(sourceImg, "E:\\Test Img\\splitimg.png", splitWidth,"jpg");
+
+		System.out.println("Sub-images have been created.");
+	}
+
+	private static void spliAndWrite(MarvinImage image, String outputPath, int splitWidth,String type) {
+		type =type==null ? "jpg":type;
+		MarvinImage splitImg = new MarvinImage(400, 400);
+		MarvinPluginCollection.crop(image, splitImg, 0, 0, splitWidth, 400);
+		MarvinPluginCollection.scale(splitImg.clone(), splitImg, 400, 400);
+		if("png".equalsIgnoreCase(type)) {
+		MarvinPluginCollection.boundaryFill(splitImg.clone(), splitImg, 0, 0, Color.white, 150);
+		splitImg.setColorToAlpha(0, 0xFFFFFFFF);
+		MarvinPluginCollection.alphaBoundary(splitImg, 5);
+		}
+		MarvinImageIO.saveImage(splitImg, outputPath+"."+type);
+	}
+
+	private static void combineAndWrite(MarvinImage image, String outputPath, int splitWidth, String type) throws IOException {
+		type =type==null ? "jpg":type;
+		MarvinImage stamp = new MarvinImage();
+		MarvinImage sign = new MarvinImage();
+
+		MarvinPluginCollection.crop(image, stamp, 0, 0, splitWidth, 400);
+		MarvinPluginCollection.crop(image, sign, stamp.getWidth(), 0, (image.getWidth() - stamp.getWidth()),
+				image.getHeight());
+		MarvinPluginCollection.blackAndWhite(stamp, 150);
+		if("png".equalsIgnoreCase(type)) {
+		MarvinPluginCollection.boundaryFill(stamp.clone(), stamp, 0, 0, Color.white, 150);
+		stamp.setColorToAlpha(0, 0xFFFFFFFF);
+		MarvinPluginCollection.alphaBoundary(stamp, 5);
+		}
+
+		MarvinPluginCollection.blackAndWhite(sign, 150);
+		if("png".equalsIgnoreCase(type)) {
+		MarvinPluginCollection.boundaryFill(sign.clone(), sign, 0, 0, Color.white, 150);
+		sign.setColorToAlpha(0, 0xFFFFFFFF);
+		MarvinPluginCollection.alphaBoundary(sign, 5);
+		}
+
+		MarvinImageIO.saveImage(stamp, "E:\\Test Img\\stamptemp.png");
+		MarvinImageIO.saveImage(sign, "E:\\Test Img\\signtemp.png");
+
+		BufferedImage combine = new BufferedImage(600, 550, BufferedImage.TYPE_INT_ARGB);
+		BufferedImage loadedStamp = stamp.getBufferedImage();
+		BufferedImage loadedSign = sign.getBufferedImage();
+
+		int x = 600 - loadedStamp.getWidth();
+		int y = 550 - (loadedSign.getHeight()+loadedStamp.getHeight());
+		System.out.println(x + ":" + y);
+		Graphics2D g2d = combine.createGraphics();
+		if(!"png".equalsIgnoreCase(type))
+		{
+		g2d.setPaint (Color.WHITE);
+		g2d.fillRect ( 0, 0, combine.getWidth(), combine.getHeight() );
+		}
+		g2d.drawImage(loadedStamp, x, y, null);
+		int x1 = 0;
+		int y1 = 550 - loadedSign.getHeight();
+		g2d.drawImage(loadedSign, x1, y1, null);
+		g2d.dispose();
+
+		ImageIO.write(combine, "png", new File("E:\\Test Img\\combine.png"));
+		
+		try {
+		File f = new File("E:\\Test Img\\stamptemp.png");
+		f.delete();
+		f = new File("E:\\Test Img\\signtemp.png");
+		f.delete();
+		} catch (Exception e) {
+			System.out.println("File deletion failed");
+		}
+	}
+
+}
---
+++ src/main/java/com/dinesh/intrvw/ImageProcessorBackup.java
@@ -0,0 +1,241 @@
+package com.dinesh.intrvw;
+
+import java.awt.AlphaComposite;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.awt.image.FilteredImageSource;
+import java.awt.image.ImageFilter;
+import java.awt.image.ImageProducer;
+import java.awt.image.RGBImageFilter;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import marvin.image.MarvinImage;
+import marvin.io.MarvinImageIO;
+import marvinplugins.MarvinPluginCollection;
+
+public class ImageProcessorBackup {
+	public static void main(String[] args) throws IOException {
+		System.setProperty("http.agent", "Chrome");
+		int imgCount = 2;
+
+//		URL url = new URL("E:\\Test Img\\Source.jpg");
+		File file = new File("E:\\Source.jpg");
+
+		// InputStream is = url.openStream();
+		BufferedImage image = ImageIO.read(file);
+
+		// initializing array to hold subimages
+		BufferedImage imgs[] = new BufferedImage[imgCount];
+
+		// Equally dividing original image into subimages
+
+		int First_img_Width = (int) Math.round(image.getWidth() * 0.3);
+		int Second_img_Width = image.getWidth() - First_img_Width;
+		int subimage_Height = image.getHeight();
+
+		
+		
+		createImage(image, imgs, First_img_Width, subimage_Height, 0, 0, 0, First_img_Width, subimage_Height);
+		createImage(image, imgs, Second_img_Width, subimage_Height, 1, First_img_Width, 0,
+				(First_img_Width + Second_img_Width), subimage_Height);
+
+		printImg(imgs, "jpg", imgCount);
+
+		printImg(imgs, "png", imgCount);
+		
+	
+		
+		System.out.println("Sub-images have been created.");
+	}
+
+	private static void printImg(BufferedImage[] imgs, String type, int imgCount) throws IOException {
+		for (int i = 0; i < imgCount; i++) {
+			StringBuilder filename = new StringBuilder();
+			filename.append("E:\\Test Img\\img").append(i).append(".").append(type);
+			File outputFile = new File(filename.toString());
+			if ("png".equalsIgnoreCase(type)) {
+			//	imgs[i] =  imageToBufferedImage(makeColorTransparent(imgs[i],Color.BLUE));
+			//	imgs[i] =  changeImageCoulor(imgs[i] );
+				printTRansparent(imgs[i], filename.toString());
+			}else {
+				ImageIO.write(convertToGrayScale(imgs[i]), type, outputFile);
+			}
+
+			
+		}
+	}
+
+	private static void createImage(BufferedImage image, BufferedImage[] imgs, int subimage_Width, int subimage_Height,
+			int current_img, int src_first_x, int src_first_y, int dst_corner_x, int dst_corner_y) {
+		imgs[current_img] = new BufferedImage(subimage_Width, subimage_Height, image.getType());
+		Graphics2D img_creator = imgs[current_img].createGraphics();
+
+		img_creator.drawImage(image, 0, 0, subimage_Width, subimage_Height, src_first_x, src_first_y, dst_corner_x,
+				dst_corner_y, null);
+	}
+
+	private static BufferedImage imageToBufferedImage(final Image image) {
+		final BufferedImage bufferedImage = new BufferedImage(image.getWidth(null), image.getHeight(null),
+				BufferedImage.TYPE_INT_ARGB);
+		final Graphics2D g2 = bufferedImage.createGraphics();
+		g2.drawImage(image, 0, 0, null);
+		g2.dispose();
+		return bufferedImage;
+	}
+
+	public static Image makeColorTransparent(Image im, final Color color) {
+
+		ImageFilter filter = new RGBImageFilter() {
+
+			public int markerRGB = color.getRGB() | 0xFFFFFFFF;
+
+			public final int filterRGB(final int x, final int y, final int rgb) {
+				if ((rgb | 0xFF000000) == markerRGB) {
+					return 0x00FFFFFF & rgb;
+				} else {
+					return rgb;
+				}
+			}
+		};
+
+		final ImageProducer ip = new FilteredImageSource(im.getSource(), filter);
+		return Toolkit.getDefaultToolkit().createImage(ip);
+	}
+
+	public static BufferedImage makeImageTranslucent(BufferedImage source, double alpha) {
+		BufferedImage target = new BufferedImage(source.getWidth(), source.getHeight(),
+				java.awt.Transparency.TRANSLUCENT);
+		// Get the images graphics
+		Graphics2D g = target.createGraphics();
+		// Set the Graphics composite to Alpha
+		g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, (float) alpha));
+		// Draw the image into the prepared reciver image
+		g.drawImage(source, null, 0, 0);
+		// let go of all system resources in this Graphics
+		g.dispose();
+		// Return the image
+		return target;
+	}
+
+	public static BufferedImage changeImageCoulor(BufferedImage img) {
+		int width = img.getWidth();
+		int height = img.getHeight();
+
+		// convert to red image
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				int p = img.getRGB(x, y);
+
+				int a = (p >> 24) & 0xff;
+				int r = (p >> 16) & 0xff;
+				  int g = (p >> 8) & 0xff;
+			        // get blue
+			        int b = p & 0xff;
+				// set new RGB keeping the r
+				// value same as in original image
+				// and setting g and b as 0.
+				 p = (a << 24) | (r << 16) | (g << 8) | b;
+			        img.setRGB(x, y, p);
+			}
+		}
+
+		return img;
+	}
+
+	public static BufferedImage convertToGrayScale(BufferedImage image) {
+
+		int width = image.getWidth();
+		int height = image.getHeight();
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				int p = image.getRGB(x, y);
+				int a = (p >> 24) & 0xff;
+				int r = (p >> 16) & 0xff;
+				int g = (p >> 8) & 0xff;
+				int b = p & 0xff;
+				int avg = (r + g + b) / 3;
+				p = (avg << 24) | (avg << 16) | (avg << 8) | avg;
+				image.setRGB(x, y, p);
+			}
+		}
+		return image;
+	}
+	
+	public static void printTRansparent(BufferedImage img,String outputString) {
+		
+		//MarvinImage mImg = MarvinImageIO.loadImage("E:\\Test Img\\Source.png");
+		MarvinImage mImg = new MarvinImage(img,"png");
+		MarvinPluginCollection.blackAndWhite(mImg, 150);
+		MarvinPluginCollection.boundaryFill(mImg.clone(), mImg, 0, 0, Color.white, 150);
+		mImg.setColorToAlpha(0, 0xFFFFFFFF);
+		MarvinPluginCollection.alphaBoundary(mImg, 5);
+		MarvinImageIO.saveImage(mImg, outputString);
+	}
+
+	
+	/* To Create two images
+	 * BufferedImage buffimgStamp = new BufferedImage(600, 550,
+	 * BufferedImage.TYPE_INT_ARGB); // Create a BufferedImage
+	 * buffimgStamp.setRGB(0, 0, 0); // with // the predefined // dimension
+	 * BufferedImage loadedStamp = stamp.getBufferedImage(); // Load the image from
+	 * file
+	 * 
+	 * int x = 600 - loadedStamp.getWidth(); // Calculate the x position for
+	 * top-right alignment int y = 0; // Calculate the y position for top-right
+	 * alignment System.out.println(x+":"+y); Graphics2D g2d =
+	 * buffimgStamp.createGraphics(); // Get the Graphics2D object of the
+	 * BufferedImage g2d.drawImage(loadedStamp, x, y, null); // Draw the loaded
+	 * image at the top-right corner of the BufferedImage g2d.dispose(); // Dispose
+	 * the Graphics2D object
+	 * 
+	 * BufferedImage buffimgSign = new BufferedImage(600, 550,
+	 * BufferedImage.TYPE_INT_ARGB); // Create a BufferedImage buffimgSign.setRGB(0,
+	 * 0, 0); // with BufferedImage loadedSign = sign.getBufferedImage();
+	 * 
+	 * int x1 = 0; // Calculate the x position for top-right alignment int y1 =
+	 * 550-loadedSign.getHeight(); // Calculate the y position for top-right
+	 * alignment System.out.println(x1+":"+y1); Graphics2D g2d2 =
+	 * buffimgSign.createGraphics(); // Get the Graphics2D object of the
+	 * BufferedImage g2d2.drawImage(loadedSign, x1, y1, null); // Draw the loaded
+	 * image at the top-right corner of the BufferedImage g2d2.dispose(); // Dispose
+	 * the Graphics2D object
+	 * 
+	 * ImageIO.write(buffimgStamp, "png", new
+	 * File("E:\\Test Img\\buffimgStamp.png")); ImageIO.write(buffimgSign, "png",
+	 * new File("E:\\Test Img\\buffimgSign.png"));
+	 */
+
+	/*
+	 * MarvinImage stampToAdd = new MarvinImage(buffimgSign); MarvinImage signToAdd
+	 * = new MarvinImage(buffimgStamp);
+	 * MarvinPluginCollection.blackAndWhite(stampToAdd,150);
+	 * MarvinPluginCollection.boundaryFill(stampToAdd.clone(), stampToAdd, 0, 0,
+	 * Color.white, 150); stampToAdd.setColorToAlpha(0, 0xFFFFFFFF);
+	 * MarvinPluginCollection.alphaBoundary(stampToAdd, 5);
+	 * MarvinPluginCollection.blackAndWhite(signToAdd,150);
+	 * MarvinPluginCollection.boundaryFill(signToAdd.clone(), signToAdd, 0, 0,
+	 * Color.white, 150); signToAdd.setColorToAlpha(0, 0xFFFFFFFF);
+	 * MarvinPluginCollection.alphaBoundary(signToAdd, 5);
+	 * 
+	 * List<MarvinImage> images = new ArrayList<MarvinImage>();
+	 * images.add(stampToAdd); images.add(signToAdd);
+	 * 
+	 * MarvinImage combine =signToAdd.clone();
+	 * MarvinPluginCollection.alphaBoundary(combine, 5);
+	 * 
+	 * //MarvinPluginCollection.mergePhotos(images, combine, 38);
+	 * MarvinPluginCollection.combineByTransparency(signToAdd, stampToAdd, combine,
+	 * 0, 0, 0);
+	 * 
+	 * MarvinImageIO.saveImage(combine, "E:\\Test Img\\combine.png");
+	 */
+
+	
+	
+}
